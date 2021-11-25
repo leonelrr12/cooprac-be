b7035eeb95e51c0a3b465e8e5d5e969e68f007cd
Leonel Rodriguez <leonellrodriguez@gmail.com>
2021-11-24 20:13:15 -0500

    -

--- src/utils/config.js
+++ src/utils/config.js
@@ -37,7 +37,7 @@ const MONGODB_URI = ""
 
 // Check connection
 cnn.connect(error => {
-  if (error) throw error;
+  // if (error) throw error;
   console.log('Database server runnuning!', cnn.host);
 })
 
