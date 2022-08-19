25815b727127e4b5733ea28fcd59c8e4367a5060
Leonel <leonellrodriguez@gmail.com>
2022-08-18 21:55:53 -0500

    Ajuste en variables de Entorno

--- src/utils/config.js
+++ src/utils/config.js
@@ -10,6 +10,7 @@ const AWS_BUCKET_REGION = process.env.AWS_BUCKET_REGION
 const AWS_Access_key_ID = process.env.AWS_Access_key_ID
 const AWS_Secret_access_key = process.env.AWS_Secret_access_key
 
+
 const MDB_HOST = process.env.MDB_HOST
 const MDB_DATABASE = process.env.MDB_DATABASE
 const MDB_USER = process.env.MDB_USER
@@ -30,9 +31,10 @@ const sendGEmail = {
   redirectUri: 'https://developers.google.com/oauthplayground'
 }
 
+
 // MongoDB DIgital Ocean-2
-//const MONGODB_URI = "mongodb+srv://doadmin:30x814oJ67N2gyYW@db-mongodb-nyc3-97071-024615bf.mongo.ondigitalocean.com/Coopracrl?authSource=admin&replicaSet=db-mongodb-nyc3-97071&tls=true&tlsCAFile=ca-certificate-MDB.crt"
-const MONGODB_URI = "mongodb+srv://doadmin:30x814oJ67N2gyYW@db-mongodb-nyc3-97071-024615bf.mongo.ondigitalocean.com/Finanservs?authSource=admin&replicaSet=db-mongodb-nyc3-97071&tls=true&tlsCAFile=ca-certificate-MDB.crt"
+//const MONGODB_URI = "mongodb+srv://doadmin:30x814oJ67N2gyYW@db-mongodb-nyc3-97071-024615bf.mongo.ondigitalocean.com/Finanservs?authSource=admin&replicaSet=db-mongodb-nyc3-97071&tls=true&tlsCAFile=ca-certificate-MDB.crt"
+const MONGODB_URI = `${MDB_PROTOCOL}://${MDB_USER}:${MDB_PWD}@${MDB_HOST}/${MDB_DATABASE}?authSource=admin&replicaSet=db-mongodb-nyc3-97071&tls=true&tlsCAFile=ca-certificate-MDB.crt`
 
 
 // MySql DIgital Ocean-2
